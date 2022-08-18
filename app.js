dc0ac86e6bc6bb316925f5066ac806d2b32fe24e
Zer0Power <zeroopoower@gmail.com>
2022-08-18 21:55:11 +0430

    add router file and changed logo

--- app.js
+++ app.js
@@ -7,3 +7,18 @@ const players = require("./src/schemas/playerSchema");
 const cors = require("cors");
 const express = require("express");
 const bodyParser = require("body-parser");
+
+const Routes = require("./routes/Routes");
+
+const app = express();
+//---------------------------💔🚬 'Zer0Power 💔🚬---------------------------//
+
+app.use(bodyParser.json());
+app.use(cors());
+
+app.listen(1010, () => {
+    console.log('[📶]Server Is Running Properly ....\n[ℹ️]Port : 1010');
+});
+
+app.use("/", Routes);
+//---------------------------💔🚬 'Zer0Power 💔🚬---------------------------//
---
+++ routes/Routes.js
@@ -0,0 +1,8 @@
+//---------------------------💔🚬 'Zer0Power 💔🚬---------------------------//
+var express = require("express"),
+  router = express.Router(),
+  players = require("../src/schemas/playerSchema");
+
+
+  module.exports = router;
+  //---------------------------💔🚬 'Zer0Power 💔🚬---------------------------//
